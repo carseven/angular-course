0a0d2ac31be0bdd82f89caff0dccc67f77e53d69
carseven <carles.serra@hotmail.es>
2022-06-12 23:39:39 +0200

    feat(countriesApp): App routing and sidebar routerLink

---
+++ countriesApp/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CapitalComponent } from './country/pages/capital/capital.component';
+import { CountryComponent } from './country/pages/country/country.component';
+import { RegionComponent } from './country/pages/region/region.component';
+import { SeeCountryComponent } from './country/pages/see-country/see-country.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CountryComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'region',
+    component: RegionComponent,
+  },
+  {
+    path: 'capital',
+    component: CapitalComponent,
+  },
+  {
+    path: 'country/:id',
+    component: SeeCountryComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- countriesApp/src/app/country/country.module.ts
+++ countriesApp/src/app/country/country.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 import { CapitalComponent } from './pages/capital/capital.component';
 import { CountryComponent } from './pages/country/country.component';
 import { RegionComponent } from './pages/region/region.component';
+import { SeeCountryComponent } from './pages/see-country/see-country.component';
 
 @NgModule({
-  declarations: [CapitalComponent, CountryComponent, RegionComponent],
+  declarations: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    SeeCountryComponent,
+  ],
   imports: [CommonModule],
-  exports: [CapitalComponent, CountryComponent, RegionComponent],
+  exports: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    SeeCountryComponent,
+  ],
 })
 export class CountryModule {}
