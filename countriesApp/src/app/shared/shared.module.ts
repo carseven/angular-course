7f1abd7426676e01ae783df70efe1c13a915a061
carseven <carles.serra@hotmail.es>
2022-06-13 19:15:16 +0200

    feat(countriesApp): Sidebar component

--- countriesApp/src/app/shared/shared.module.ts
+++ countriesApp/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   exports: [SidebarComponent],
 })
 export class SharedModule {}
