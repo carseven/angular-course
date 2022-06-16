da8900030da32afcbf6fc128f535c27b1f22f525
carseven <carles.serra@hotmail.es>
2022-06-16 18:29:34 +0200

    feat()

--- countriesApp/src/app/country/pages/region/region.component.ts
+++ countriesApp/src/app/country/pages/region/region.component.ts
@@ -1,12 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountryResponse } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-region',
   templateUrl: './region.component.html',
-  styles: [],
-})
-export class RegionComponent implements OnInit {
-  constructor() {}
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+        margin-top: 5px;
+      }
 
-  ngOnInit(): void {}
+      .active-btn {
+      }
+    `,
+  ],
+})
+export class RegionComponent {
+  readonly regions: [string, string][] = [
+    ['EU', ' European Union'],
+    ['EFTA', ' European Free Trade Association'],
+    ['CARICOM', ' Caribbean Community'],
+    ['PA', ' Pacific Alliance'],
+    ['AU', ' African Union'],
+    ['USAN', ' Union of South American Nations'],
+    ['EEU', ' Eurasian Economic Union'],
+    ['AL', ' Arab League'],
+    ['ASEAN', ' Association of Southeast Asian Nations'],
+    ['CAIS', ' Central American Integration System'],
+    ['CEFTA', ' Central European Free Trade Agreement'],
+    ['NAFTA', ' North American Free Trade Agreement'],
+    ['SAARC', ' South Asian Association for Regional Cooperation'],
+  ];
+
+  countries: CountryResponse[] = [];
+  selectedRegion: string = '';
+
+  constructor(private readonly countryService: CountryService) {}
+  searchRegion(region: [string, string]) {
+    this.selectedRegion = region[0];
+
+    this.countryService
+      .getCountriesByRegionalBloc(this.selectedRegion)
+      .subscribe((response: CountryResponse[]) => (this.countries = response));
+  }
 }
--- countriesApp/src/app/country/services/country.service.ts
+++ countriesApp/src/app/country/services/country.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
 import { CountryResponse } from '../interfaces/country.interface';
@@ -14,7 +14,7 @@ export class CountryService {
   searchCountriesByName(countryQuery: string): Observable<CountryResponse[]> {
     const url: string = `${this.apiUrl}/name/${countryQuery}`;
     return this.httpCliente
-      .get<CountryResponse[]>(url)
+      .get<CountryResponse[]>(url, { params: this.getParamsTable() })
       .pipe(catchError(() => of<CountryResponse[]>([])));
   }
 
@@ -23,14 +23,27 @@ export class CountryService {
   ): Observable<CountryResponse[]> {
     const url: string = `${this.apiUrl}/capital/${capitalCityQuery}`;
     return this.httpCliente
-      .get<CountryResponse[]>(url)
+      .get<CountryResponse[]>(url, { params: this.getParamsTable() })
       .pipe(catchError(() => of<CountryResponse[]>([])));
   }
 
   getCountryByAlphaId(alphaId: string): Observable<CountryResponse | null> {
     const url: string = `${this.apiUrl}/alpha/${alphaId}`;
     return this.httpCliente
-      .get<CountryResponse | null>(url)
+      .get<CountryResponse | null>(url, { params: this.getParamsTable() })
       .pipe(catchError(() => of(null)));
+  }
+
+  getCountriesByRegionalBloc(
+    regionalBloc: string
+  ): Observable<CountryResponse[]> {
+    const url: string = `${this.apiUrl}/regionalbloc/${regionalBloc}`;
+    return this.httpCliente
+      .get<CountryResponse[]>(url, { params: this.getParamsTable() })
+      .pipe(catchError(() => of<CountryResponse[]>([])));
+  }
+
+  private getParamsTable(): HttpParams {
+    return new HttpParams().set('fiels', 'name,alpha2code,flag,population');
   }
 }
