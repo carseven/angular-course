67a84d435dc21029d1ee1ef0a98208cf8c3f94c0
carseven <carles.serra@hotmail.es>
2022-07-16 12:25:38 +0200

    fix(graphsApp): FIx netlifly rounting problem

--- graphsApp/src/app/app-routing.module.ts
+++ graphsApp/src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'graficas',
-    loadChildren: () => import('./graficas/graficas.module').then( m => m.GraficasModule )
+    loadChildren: () =>
+      import('./graficas/graficas.module').then((m) => m.GraficasModule),
   },
   {
     path: '**',
-    redirectTo: 'graficas'
-  }
+    redirectTo: 'graficas',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
